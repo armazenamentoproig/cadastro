e84813027a8a2311f311aeddb2b6f1a0fa262cce
armazenamentoproig <edinaldobarbosa@edu.petrolina.g12.br>
2023-02-06 21:38:53 -0300

    Atualizar arquivos

--- conexao.php
+++ conexao.php
@@ -1,18 +1,6 @@
 <?php
-
-$servidor = "aws-sa-east-1.connect.psdb.cloud";
-$usuario = "ioudoj7c787zzow91xbp"; 			                               //Necessário inserir nome de usuario do Mysql entre aspas
-$senha = "pscale_pw_b9NXLvxy1gtj3EJA7JrPitgv48QPwqeXjm03FzDKjH8";			//Necessário inserir senha de usuario utilizado do Mysql entre aspas
-$dbname = "formulario";	//Informar o nome do banco de dados
-
-//Criar a conexao
-
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
-if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-}
-
-echo "<h1>Conexão bem sucedida</h1>";
-
+  $mysqli = mysqli_init();
+  $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+  $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+  $mysqli->close();
 ?>
